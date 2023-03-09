880481e3b02f1ef2b7512e9383d7664c48914ee9
DESKTOP-A310VE7\Jacob <jacobmnilsen@gmail.com>
2023-03-09 23:43:31 +0100

    Added rate limiter to the recycling machine

--- src/main/java/com/jacob/panteautomat/controller/RecyclingMachineController.java
+++ src/main/java/com/jacob/panteautomat/controller/RecyclingMachineController.java
@@ -1,10 +1,15 @@
 package com.jacob.panteautomat.controller;
 
 import com.jacob.panteautomat.service.RecyclingMachineService;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Duration;
 
 @RestController
 @CrossOrigin(maxAge = 3600)
@@ -12,28 +17,51 @@ public class RecyclingMachineController {
 
     Logger logger = LoggerFactory.getLogger(RecyclingMachineController.class);
 
+    private final Bucket canBucket;
+    private final Bucket bottleBucket;
+
     private final RecyclingMachineService recyclingMachineService = new RecyclingMachineService();
+
+    public RecyclingMachineController() {
+        Bandwidth canRequestLimit = Bandwidth.classic(1, Refill.intervally(1, Duration.ofMillis(500)));
+        this.canBucket = Bucket.builder()
+                .addLimit(canRequestLimit)
+                .build();
+
+        Bandwidth bottleRequestLimit = Bandwidth.classic(1, Refill.intervally(1, Duration.ofSeconds(1)));
+        this.bottleBucket = Bucket.builder()
+                .addLimit(bottleRequestLimit)
+                .build();
+    }
 
 
     @PostMapping("/cans")
     @ResponseStatus(code = HttpStatus.ACCEPTED)
     public String insertCan() {
-        String canMessage = "Can inserted. ";
+        if(canBucket.tryConsume(1)) {
+            String canMessage = "Can inserted. ";
 
-        recyclingMachineService.insertCan();
+            recyclingMachineService.insertCan();
 
-        logger.info(canMessage + recyclingMachineService.getRecycledAmount());
-        return canMessage;
+            logger.info(canMessage + recyclingMachineService.getRecycledAmount());
+            return canMessage;
+        }
+        logger.warn("Can inserted too fast!");
+        return "You inserted a can too fast!";
     }
     @PostMapping("/bottles")
     @ResponseStatus(code = HttpStatus.ACCEPTED)
     public String insertBottle() {
-        String bottleMessage = "Bottle inserted. ";
+        if (bottleBucket.tryConsume(1)) {
+            String bottleMessage = "Bottle inserted. ";
 
-        recyclingMachineService.insertBottle();
+            recyclingMachineService.insertBottle();
 
-        logger.info(bottleMessage + recyclingMachineService.getRecycledAmount());
-        return bottleMessage;
+            logger.info(bottleMessage + recyclingMachineService.getRecycledAmount());
+            return bottleMessage;
+        }
+        logger.warn("Bottle inserted too fast");
+        return "You inserted a bottle too fast!";
     }
 
     @GetMapping("/recycledamount")
